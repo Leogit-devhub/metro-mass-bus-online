faab8c930d933120b2cfadb7761b77983f3cbcfe
DOMINIC MAABOBRA TUOLONG <101972263+Leogit-devhub@users.noreply.github.com>
2022-10-20 08:46:23 +0000

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -5,7 +5,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x@w#-g%axp02y%dc^&#a2(+9s_*&oe*b-0ssdorgigpocs@rqr'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True 
